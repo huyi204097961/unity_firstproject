b7ebf609da7d8759afa6a40aa67336a77c3d2aea
huyi204097961 <huyi204097961@gmail.com>
2025-03-12 21:08:44 +0800

    初始化

---
+++ Assets/movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float movespeed = 3f;
+    public Rigidbody2D rb;
+    private Vector2 dir;
+    public Animator animator;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = dir * movespeed * 20;
+        rb.MovePosition(rb.position + dir * movespeed * Time.fixedDeltaTime);
+        animator.SetFloat("speed", rb.velocity.magnitude);
+        if(dir.x>0)
+        transform.localScale = new Vector2(1, 1);
+        else if (dir.x < 0)
+        transform.localScale = new Vector2(-1, 1);
+        
+    }
+}
